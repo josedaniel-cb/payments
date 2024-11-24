5e9301c7e2f235a2299f4c5de774097ec98e0300
Daniel Calle Briceño <j.daniel.c137@gmail.com>
2024-11-24 02:38:56 +0000

    refactor: format

--- payments/payment_gateways/doctype/izipay_settings/test_izipay_settings.py
+++ payments/payment_gateways/doctype/izipay_settings/test_izipay_settings.py
@@ -1,7 +1,5 @@
-# Copyright (c) 2018, Frappe Technologies and Contributors
-# License: MIT. See LICENSE
 import unittest
 
 
-class TestStripeSettings(unittest.TestCase):
-	pass
+class TestIzipaySettings(unittest.TestCase):
+    pass
